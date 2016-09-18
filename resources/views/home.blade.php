638c6d62e6ab6667ba785d7887059496518186f1
Behnam Baharmand <behnam@baharmand.com>
2016-09-18 15:08:13 +0430

    Update App Name and layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,14 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+
+                <h1>{{ Auth::user()->name }}</h1>
+                
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    You are logged in, {{ Auth::user()->name }}!
                 </div>
             </div>
         </div>
