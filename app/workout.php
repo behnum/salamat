c48aa2c131987e38e794a2d5d03d6ca56efd4e68
Behnam Baharmand <behnam@baharmand.com>
2016-10-03 18:07:58 +0330

    ** Fixing Relationships between Users and Workouts[D[D

--- app/workout.php
+++ app/workout.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class workout extends Model
+class Workout extends Model
 {
-    //
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+      'user_id', 'date', 'active_cal', 'avg_pace', 'distance', 'weight'
+  ];
+
+  public function user()
+  {
+    return $this->belongsTo('App\User', 'user_id');
+  }
 }
